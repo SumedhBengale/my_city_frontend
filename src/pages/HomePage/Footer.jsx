d52c62b6c6655d8783745e2bc242a37475ca6010
Sumedh Bengale <ssbengale@gmail.com>
2023-07-07 10:44:20 +0530

    Homepage design completed, date-picker, etc implementation remaining

--- src/pages/HomePage/Footer.jsx
+++ src/pages/HomePage/Footer.jsx
@@ -1,5 +1,12 @@
 import React from 'react'
 import black_logo from '../../assets/images/black_logo.png'
+import location_black from '../../assets/images/home/location_black.svg'
+import phone_black from '../../assets/images/home/phone_black.svg'
+import email_black from '../../assets/images/home/email_black.svg'
+import twitter from '../../assets/images/home/twitter.svg'
+import facebook from '../../assets/images/home/facebook.svg'
+import instagram from '../../assets/images/home/instagram.svg'
+import linkedin from '../../assets/images/home/linkedin.svg'
 
 function Footer() {
   return (
@@ -14,7 +21,7 @@ function Footer() {
         <div className='pt-10 pl-5'>
             <div className='text-xl font-bold'>Explore</div>
             <div className="text-black text-md pt-3 pl-3">Apartment</div>
-            <div className="text-black text-md pt-3 pl-3">Concominimum</div>
+            <div className="text-black text-md pt-3 pl-3">Condominimum</div>
         </div>
 
         <div className='pt-10 pl-5'>
@@ -27,8 +34,58 @@ function Footer() {
         </div>
 
         <div className='pt-10 pl-5'>
-            <div className='text-xl font-bold'>Contact Us</div>
+            <div className='text-xl font-bold w-full'>Contact Us</div>
+                <div className=' flex items-center h-full mt-5'>
+                    <img src={location_black} alt='location' className='w-10 pr-3'></img>
+                    <div className=" w-full text-black text-md flex items-center">Po Box 1046, Stockton On Tees, TS19 1XL</div>
+                </div>
+
+                <div className='mr-5'>
+                    <div className="w-full h-[1px] bg-black mt-3 mr-20" />
+                </div>
+
+                <div className='flex items-center h-full mt-5'>
+                    <img src={phone_black} alt='location' className='w-10 pr-3'></img>
+                    <div className=" w-full text-black text-md flex items-center">800 987 6543</div>
+                </div>
+
+                <div className='mr-5'>
+                    <div className="w-full h-[1px] bg-black mt-3 mr-20" />
+                </div>
+
+                <div className='flex items-center h-full mt-5'>
+                    <img src={email_black} alt='location' className='w-10 pr-3'></img>
+                    <div className=" w-full text-black text-md flex items-center">info@boostly.co.uk</div>
+                </div>
+
+                <div className='mr-5'>
+                    <div className="w-full h-[1px] bg-black mt-3 mr-20" />
+                </div>
         </div>
+
+
+        <div className='flex justify-evenly mt-5'>
+            <div className='w-8 h-8 rounded-full border-[2px] border-black'>
+                <img src={twitter} alt='twitter' className='w-5 h-5 ml-1 mt-1'></img>
+            </div>
+
+            <div className='w-8 h-8 rounded-full border-[2px] border-black'>
+                <img src={facebook} alt='twitter' className='w-5 h-5 ml-1 mt-1'></img>
+            </div>
+
+            <div className='w-8 h-8 rounded-full border-[2px] border-black'>
+                <img src={instagram} alt='twitter' className='w-5 h-5 ml-1 mt-1'></img>
+            </div>
+
+            <div className='w-8 h-8 rounded-full border-[2px] border-black'>
+                <img src={linkedin} alt='twitter' className='w-5 h-5 ml-1 mt-1'></img>
+            </div>
+        </div>
+
+        <div className='flex justify-center mt-5 text-sm'>
+            My City Residence ©2023. All rights reserved.
+        </div>
+
     </div>
     </>
   )
