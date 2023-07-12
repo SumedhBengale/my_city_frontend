bdb6d37d61bed30093c6e0d6562992062fee6dea
Sumedh Bengale <ssbengale@gmail.com>
2023-07-12 22:44:31 +0530

    Fixed warning in date picker that caused build to crash

--- src/components/CustomDatePicker.js
+++ src/components/CustomDatePicker.js
@@ -10,20 +10,6 @@ const CustomDatePicker = ({ selectedDate, setSelectedDate, onClickOutside }) => 
 
 
   const weekdaysShort = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-  const months = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-    'July',
-    'August',
-    'September',
-    'October',
-    'November',
-    'December',
-  ];
 
   const handleDateClick = (day) => {
       console.log("handleDateClick")
