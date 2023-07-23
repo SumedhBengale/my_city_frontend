4e6669110cadddf392f6d16918403029c955aff3
Sumedh Bengale <ssbengale@gmail.com>
2023-07-23 23:52:27 +0530

    Updates as per recommendations

--- src/pages/Management/ManagementPage.jsx
+++ src/pages/Management/ManagementPage.jsx
@@ -146,37 +146,40 @@ function ManagementPage() {
       <div className='text-2xl font-custom font-bold text-black text-center mt-10 mx-2'>Let Us Handle It All</div>
 
       <div className="w-full pr-8 pt-16 relative lg:pr-20">
-      <div className='w-full h-56 md:h-[400px] rounded-r-[50px] md:rounded-r-[150px]'>
+      <div className='w-full h-64 md:h-[400px] rounded-r-[50px] md:rounded-r-[150px] relative'>
           <img src={Image5} alt="About Us" className="w-full h-full md:object-fill lg:object-cover rounded-r-[50px]  md:rounded-r-[150px] " />
+          <div className='absolute top-0  w-full h-full md:object-fill lg:object-cover rounded-r-[50px]  md:rounded-r-[150px] bg-black/30'></div>
         </div>
-          <div className='absolute top-8 h-full flex items-center justify-center pr-5'>
-            <div className=" w-4/5 md:w-1/2 lg:w-2/3 md:py-10 h-max bg-white bg-opacity-5 rounded-2xl border border-white backdrop-blur-md">
-              <div className=" w-full text-center text-white font-custom font-bold text-sm md:text-xl px-2 pb-1">Management that is simple</div>
-              <div className=" w-full text-center text-white text-[10px] sm:text-lg px-2 font-normal pb-2 ">Our experienced staff will examine the condition, location, accessibility, and zoning rules of the property. We’ll discuss contracts and locate one that meets your demands once we’ve determined the financial sustainability of your property.</div>
+          <div className='absolute top-8 h-full w-full flex items-center justify-center pr-5'>
+            <div className=" w-4/5 md:w-1/2 lg:w-2/3 md:py-10 h-max flex flex-col gap-3">
+              <div className=" w-full text-center text-white font-custom font-bold text-lg md:text-xl px-2 pb-1">Management that is simple</div>
+              <div className=" w-full text-center text-white text-[12px] font-bold sm:text-lg px-2 pb-2 ">Our experienced staff will examine the condition, location, accessibility, and zoning rules of the property. We’ll discuss contracts and locate one that meets your demands once we’ve determined the financial sustainability of your property.</div>
             </div>
           </div>
         </div>
 
         <div className="w-full pl-8 pt-16 relative lg:pl-20">
-      <div className='w-full h-56 md:h-[400px] rounded-l-[50px] md:rounded-l-[150px]'>
+        <div className='w-full h-64 md:h-[400px] rounded-l-[50px] md:rounded-l-[150px] relative'>
           <img src={Image6} alt="About Us" className="w-full h-full md:object-fill lg:object-cover rounded-l-[50px]  md:rounded-l-[150px] " />
+          <div className='absolute top-0 w-full h-full md:object-fill lg:object-cover rounded-l-[50px]  md:rounded-l-[150px] bg-black/30'></div>
         </div>
-          <div className='absolute top-8 h-full flex items-center justify-center pl-5'>
-            <div className=" w-4/5 md:w-1/2 lg:w-2/3 md:py-10 h-max bg-white bg-opacity-5 rounded-2xl border border-white backdrop-blur-md">
-              <div className=" w-full text-center text-white font-custom font-bold text-sm md:text-xl px-2 pb-1">Verified Tenants</div>
-              <div className=" w-full text-center text-white text-[10px] sm:text-lg px-2 font-normal pb-2 ">We have been landlords and agents for a long period of time and understand how important it is to pick the finest quality renters for the property. Our renters are all professionals who have been carefully verified before moving in.</div>
+        <div className='absolute top-8 h-full flex items-center justify-center pl-5'>
+            <div className=" w-4/5 md:w-1/2 lg:w-2/3 md:py-10 h-max flex flex-col gap-3">
+              <div className=" w-full text-center text-white font-custom font-bold text-lg md:text-xl px-2 pb-1">Verified Tenants</div>
+              <div className=" w-full text-center text-white text-[12px] font-bold sm:text-lg px-2 pb-2 ">We have been landlords and agents for a long period of time and understand how important it is to pick the finest quality renters for the property. Our renters are all professionals who have been carefully verified before moving in.</div>
             </div>
           </div>
         </div>
 
         <div className="w-full pr-8 pt-16 relative lg:pr-20">
-      <div className='w-full h-56 md:h-[400px] rounded-r-[50px] md:rounded-r-[150px]'>
-          <img src={Image7} alt="About Us" className="w-full h-full md:object-fill lg:object-cover rounded-r-[50px] md:rounded-r-[150px] " />
+        <div className='w-full h-64 md:h-[400px] rounded-r-[50px] md:rounded-r-[150px] relative'>
+          <img src={Image7} alt="About Us" className="w-full h-full md:object-fill lg:object-cover rounded-r-[50px]  md:rounded-r-[150px] " />
+          <div className='absolute top-0  w-full h-full md:object-fill lg:object-cover rounded-r-[50px]  md:rounded-r-[150px] bg-black/30'></div>
         </div>
-          <div className='absolute top-8 h-full flex items-center justify-center pr-5'>
-            <div className=" w-4/5 md:w-1/2 lg:w-2/3 md:py-10 h-max bg-white bg-opacity-5 rounded-2xl border border-white backdrop-blur-md">
-              <div className=" w-full text-center text-white font-custom font-bold text-sm md:text-xl px-2 pb-1">We Handle all the Tenants</div>
-              <div className=" w-full text-center text-white text-[10px] sm:text-lg px-2 font-normal pb-2 ">No more worrying about another late-night call from your renter. Our customer support representatives are in charge of all setup, daily operations, and guest communications.</div>
+        <div className='absolute top-8 h-full w-full flex items-center justify-center pr-5'>
+            <div className=" w-4/5 md:w-1/2 lg:w-2/3 md:py-10 h-max flex flex-col gap-3">
+              <div className=" w-full text-center text-white font-custom font-bold text-lg md:text-xl px-2 pb-1">We Handle all the Tenants</div>
+              <div className=" w-full text-center text-white text-[12px] font-bold sm:text-lg px-2 pb-2 ">No more worrying about another late-night call from your renter. Our customer support representatives are in charge of all setup, daily operations, and guest communications.</div>
             </div>
           </div>
         </div>
