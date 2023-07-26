b740c83712d0897740780920655750ffc657390a
Sumedh Bengale <ssbengale@gmail.com>
2023-07-26 21:07:06 +0530

    Removed the api integration

--- src/App.js
+++ src/App.js
@@ -19,22 +19,19 @@ import ScrollToTop from './components/ScrollToTop';
 
 
 const App = () => {
-  const getToken = () => { localStorage.getItem('token') };
+  // const getToken = () => { localStorage.getItem('token') };
   return (
     <Router>
       <ScrollToTop>
         <Routes>
-          <Route exact path="/" element={
-            //check whether the user is logged in or not
-            getToken ? <HomePage></HomePage> : <Navigate to="/login"></Navigate>
-          } />
+          <Route exact path="/" element={<HomePage></HomePage>} />
           <Route exact path="/login" element={<LoginPage></LoginPage>} />
           <Route exact path= "/signup" element={<SignUpPage></SignUpPage>} />
           <Route exact path="/properties" element={<PropertiesPage></PropertiesPage>} />
           <Route exact path="/management" element={<ManagementPage></ManagementPage>} />
           <Route exact path="/about" element={<AboutUsPage></AboutUsPage>} />
           <Route exact path="/contact" element={<ContactUsPage></ContactUsPage>} />
-          <Route exact path="/property/:id" element={<Property></Property>} />
+          <Route exact path="/property/" element={<Property></Property>} />
           <Route exact path='/wishlist' element={<Wishlist></Wishlist>} />
           <Route exact path='/book' element={<Book></Book>} />
           <Route exact path="/trips" element={<Trips></Trips>} />
--- src/pages/HomePage/PropertyCard.jsx
+++ src/pages/HomePage/PropertyCard.jsx
@@ -5,40 +5,40 @@ import shower from '../../assets/images/home/shower.svg'
 import person from '../../assets/images/home/person.svg'
 import { useNavigate } from 'react-router-dom'
 
-function PropertyCard({residence}) {
+function PropertyCard() {
   const navigate = useNavigate();
   return (
-    <div className=" w-full md:max-w-[600px] max-w-[400px] h-min bg-gray-200 hover:bg-gray-300 transition duration-75 rounded-2xl backdrop-blur-[185px] pb-5 mb-5" onClick={()=>navigate(`/property/${residence._id}`)}>
+    <div className=" w-full md:max-w-[600px] max-w-[400px] bg-gray-200 hover:bg-gray-300 transition duration-75 rounded-2xl backdrop-blur-[185px] pb-5 mb-5" onClick={()=>navigate('/property')}>
         <div className='relative w-full'>
           <div className="absolute bottom-7 left-3 rounded-full pl-4 text-sm font-semibold text-white">
-          <div className="flex"><div className="text-white text-xl -translate-y-2 font-normal">€</div><div className="text-white text-xl font-bold">{residence.pricePerNight}</div><div className="text-white text-sm font-normal self-end">/night</div></div>
+          <div className="flex"><div className="text-white text-xl -translate-y-2 font-normal">£</div><div className="text-white text-xl font-bold">500</div><div className="text-white text-sm font-normal self-end">/night</div></div>
           </div>
           <div className='w-full p-4'>
-            <img className="w-full h-48 lg:h-56 rounded-xl object-cover" src={residence.images[0]} alt="Placeholder"/>
+            <img className="w-full rounded-xl" src={placeholderRoomImage} alt="Placeholder"/>
           </div>
         </div>
         <div className="px-6 py-3">
-          <div className="font-bold font-custom text-2xl mb-2 overflow-ellipsis line-clamp-1">{residence.title}</div>
-          <p className="text-black line-clamp-1">{residence.address}</p>
+          <div className="font-bold font-custom text-2xl mb-2 overflow-ellipsis line-clamp-1">Amazing Highly Spacious</div>
+          <p className="text-black">Lyall St, London SW1X 8DW, UK</p>
         </div>
 
     {/*Also fetch these values from the API */}
-    <div className='grid grid-cols-1 xs:grid-cols-2 px-5 gap-2'>
-        <div className='flex justify-start gap-1'>
+    <div className='flex justify-between px-5'>
+        <div className='flex justify-between gap-4'>
             <div className='flex'>
                 <img src={bed} alt='bed' className=''></img>
-                <div className='pl-1'>{residence.beds}</div>
+                <div className='pl-2'>1</div>
             </div>
             <div className='flex'>
                 <img src={shower} alt='shower' className=''></img>
-                <div className='pl-1'>{residence.bathrooms}</div>
+                <div className='pl-2'>1</div>
             </div>
             <div className='flex'>
                 <img src={person} alt='person' className=''></img>
-                <div className='pl-1'>{residence.guests}</div>
+                <div className='pl-2'>1</div>
             </div>
         </div>
-        <div className='text-md xs:text-end'>{residence.type}</div>
+        <div className='text-md'>Condominimum</div>
     </div>
   </div>
   )
