f6d2d78899506c9f583995cee46b50bdeffe8b4d
Ubuntu <ubuntu@ip-172-31-35-70.eu-north-1.compute.internal>
2023-10-07 14:34:20 +0000

    Final Commit

--- src/config/config.prod.js
+++ src/config/config.prod.js
@@ -2,7 +2,7 @@ const config = {
     API_URL: 'https://mycityresidences.com/api',
     STRAPI_URL: 'https://mycityresidences.com/strapi',
     MAPS_API_KEY: 'AIzaSyBoZFGXOCwer9dv34IPMOhFqlApLBQtprs',
-    STRIPE_PUBLISHABLE_KEY: 'pk_test_51NaN27KRJu2e9UD4Y8LuP52Mlb6VyUFTjfsLKCOsU66F80zhBNpfA1IKh0CnEBxYZsg645Kqzr2NLgj9DIjhnHzi00kJPJxInm'
+    STRIPE_PUBLISHABLE_KEY: 'pk_live_51NaN27KRJu2e9UD42kJNTEAU88wAFxMWN3eatJhAK0P3ZyUvlgUBoFkznYEDfzJ2JSP6VRJtvk8Y7K1sxwEBNKHw00vkDT0Rjr'
 }
 
 export default config;
